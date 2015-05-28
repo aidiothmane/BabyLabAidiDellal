5f4066a6f191d0d95c1f1824eafbab3b54ff5d3f
Aidi othmane <aidi.othmane@gmail.com>
2015-05-28 13:05:46 +0200

    Ajout des fonctionalités + des frames

--- src/babylabaididellal/BabyLabAidiDellal.java
+++ src/babylabaididellal/BabyLabAidiDellal.java
@@ -4,6 +4,11 @@
  */
 package babylabaididellal;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Aidi Othmane
@@ -13,6 +18,31 @@ public class BabyLabAidiDellal {
     /**
      * @param args the command line arguments
      */
+     public String[] parserFichier(String url){
+                String chaine="";
+                int i = 0;
+		String fichier =url;
+		
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				//System.out.println(ligne);
+				chaine+=ligne+"\n";
+			}
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+                String[] sortie = chaine.split("\n\n");
+                System.out.println(chaine);
+                return sortie;
+                
+}
     public static void main(String[] args) {
         // TODO code application logic here
     }
